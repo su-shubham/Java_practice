8373712f93a547f13f113232cdd118383db67e3d
su Shubham[0] <75021117+shubham8268@users.noreply.github.com>
2022-01-19 10:01:16 +0000

    update and created student details for user input

--- user_input.java
+++ user_input.java
@@ -4,10 +4,20 @@ class user_input
 {
   public static void main(String[] args)
   {
-  System.out.println("Enter the Name:");
+    System.out.println("----Enter The Student details----");
   Scanner user= new Scanner(System.in);
+  System.out.println("Enter the Name:");
   String name=user.nextLine();
+  System.out.println("Enter The id:");
+  int id=user.nextInt();
+  user.nextLine();
+  System.out.println("Enter The college Name: ");
+  String college=user.nextLine();
+  System.out.println("Name: "+name);
+  System.out.println("Id: "+id);
+   System.out.println("College Name: "+college);
+   System.out.println("Thankyou!");
 
-  System.out.println("Name is "+name);
+    
   }
 }
